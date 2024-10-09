e63386ac9eebaa08ac972079d64bd9d2cd9941e6
Aditya Saha <aadi@Adityas-MacBook-Air.local>
2024-10-09 23:39:12 +0530

    first commit

---
+++ src/main/java/com/foodorder/foodcatalogue/mapper/FoodItemMapper.java
@@ -0,0 +1,18 @@
+package com.foodorder.foodcatalogue.mapper;
+
+import com.foodorder.foodcatalogue.dto.FoodItemDTO;
+import com.foodorder.foodcatalogue.entity.FoodItem;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface FoodItemMapper {
+
+    FoodItemMapper INSTANCE = Mappers.getMapper(FoodItemMapper.class);
+
+    FoodItem mapFoodItemDTOToFoodItem(FoodItemDTO foodItemDTO);
+
+    FoodItemDTO mapFoodItemToFoodItemDto(FoodItem foodItem);
+
+
+}
